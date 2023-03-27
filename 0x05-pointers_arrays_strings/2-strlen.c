4f7b8707531a6a37ceefb9aeb7c551ec5ef1c794
Jolleymercy <jolleymercy@gmail.com>
2023-03-27 10:21:39 -0700

    strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenth of a string
+ * @s: String
+ * Return: the length of the string
+ */
+int _strlen(char *S)
+{
+	int longi = 0;
+
+	while(*S != '\0')
+	{
+		longi++;
+		S++;
+	}
+
+	return (longi);
+}
